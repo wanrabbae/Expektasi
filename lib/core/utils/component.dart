800f8485b42a86a20657653f71f1c796f4102aa9
wanrabbae <wancode.msg@gmail.com>
2022-08-08 13:08:12 +0700

    Build Profile Screen

--- lib/core/utils/component.dart
+++ lib/core/utils/component.dart
@@ -1,84 +1,77 @@
-
 import 'package:flutter/material.dart';
 import 'package:phosphor_flutter/phosphor_flutter.dart';
-
 
 Color secondaryColor = Color(0xffFDD100);
 
 AppBar customAppBar([PreferredSizeWidget? bottom]) => AppBar(
-  backgroundColor: secondaryColor,
-  title: Container(
-    width: double.infinity,
-    height: 40,
-    decoration: BoxDecoration(
-        color: Colors.white, borderRadius: BorderRadius.circular(5)),
-    child: Center(
-      child: TextFormField(
-        decoration: InputDecoration(
-          contentPadding: EdgeInsets.symmetric(
-            vertical: 12.5
-          ),
-            fillColor: Colors.white,
-            prefixIcon: const Icon(Icons.search),
-            suffixIcon: IconButton(
-              icon: const Icon(Icons.camera_alt),
-              onPressed: () {
-                /* Clear the search field */
-              },
+      backgroundColor: secondaryColor,
+      title: Container(
+        width: double.infinity,
+        height: 40,
+        decoration: BoxDecoration(
+            color: Colors.white, borderRadius: BorderRadius.circular(5)),
+        child: Center(
+          child: TextFormField(
+            decoration: InputDecoration(
+              contentPadding: EdgeInsets.symmetric(vertical: 12.5),
+              fillColor: Colors.white,
+              prefixIcon: const Icon(Icons.search),
+              suffixIcon: IconButton(
+                icon: const Icon(Icons.camera_alt),
+                onPressed: () {
+                  /* Clear the search field */
+                },
+              ),
+              hintText: 'Hari ini mau nyari apa nih?..',
+              border: InputBorder.none,
             ),
-            hintText: 'Hari ini mau nyari apa nih?..',
-          border: InputBorder.none,
+          ),
         ),
       ),
-    ),
-  ),
-  actions: [
-    IconButton(
-        onPressed: (){},
-        icon: Icon(
-          PhosphorIcons.bellSimple,
-          color: Colors.white,
+      actions: [
+        IconButton(
+          onPressed: () {},
+          icon: Icon(
+            PhosphorIcons.bellSimple,
+            color: Colors.white,
+          ),
         ),
-    ),
-    IconButton(
-        onPressed: (){},
+        IconButton(
+          onPressed: () {},
           icon: Icon(
             PhosphorIcons.chats,
             color: Colors.white,
           ),
-    ),
-  ],
-  elevation: 3,
-  toolbarHeight: 80,
-);
+        ),
+      ],
+      elevation: 3,
+      toolbarHeight: 80,
+    );
 AppBar customAppBar2([PreferredSizeWidget? bottom]) => AppBar(
-  backgroundColor: Colors.white,
-  elevation: 2,
-  title: Center(
-    child: Text(
-      'Keranjang Belanja',
-      style: TextStyle(
-        fontSize: 22,
-        fontWeight: FontWeight.w500,
-        color: Colors.black
+      backgroundColor: Colors.white,
+      elevation: 2,
+      title: Center(
+        child: Text(
+          'Keranjang Belanja',
+          style: TextStyle(
+              fontSize: 22, fontWeight: FontWeight.w500, color: Colors.black),
+        ),
       ),
-    ),
-  ),
-  bottom: TabBar(
-    tabs: [
-      Tab(
-        text: 'Semua',
+      bottom: TabBar(
+        tabs: [
+          Tab(
+            text: 'Semua',
+          ),
+          Tab(
+            text: 'Favorit',
+          ),
+          Tab(
+            text: 'Beli Lagi',
+          ),
+        ],
       ),
-      Tab(
-        text: 'Favorit',
-      ),
-      Tab(
-        text: 'Beli Lagi',
-      ),
-    ],
-  ),
-  toolbarHeight: 80,
-);
+      toolbarHeight: 80,
+    );
 
 DefaultTabController customAppbar3() => DefaultTabController(
     length: 3,
@@ -90,10 +83,7 @@ DefaultTabController customAppbar3() => DefaultTabController(
           child: Text(
             'Keranjang Belanja',
             style: TextStyle(
-                fontSize: 22,
-                fontWeight: FontWeight.w500,
-                color: Colors.black
-            ),
+                fontSize: 22, fontWeight: FontWeight.w500, color: Colors.black),
           ),
         ),
         bottom: TabBar(
@@ -110,5 +100,4 @@ DefaultTabController customAppbar3() => DefaultTabController(
           ],
         ),
       ),
-    )
-);
+    ));
