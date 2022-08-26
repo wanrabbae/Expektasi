7ca83258dfa6793f60233ab471298815fd4ec253
wanrabbae <wancode.msg@gmail.com>
2022-08-26 14:49:05 +0700

    Add rincian order

--- lib/views/home/profile/riwayat.dart
+++ lib/views/home/profile/riwayat.dart
@@ -1,4 +1,5 @@
 import 'package:expektasi/core/utils/component.dart';
+import 'package:expektasi/core/utils/constant.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
@@ -28,19 +29,103 @@ class _RiwayatViewState extends State<RiwayatView> {
             onPressed: () => Navigator.pop(context),
           ),
         ),
-        body: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisAlignment: MainAxisAlignment.center,
+        body: SingleChildScrollView(
+            child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          // mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Center(
-              child: Icon(
-                PhosphorIcons.clockCounterClockwiseBold,
-                size: 200,
-                color: secondaryColor,
+            Container(
+              width: double.infinity,
+              height: 1000,
+              padding: EdgeInsets.symmetric(horizontal: 5),
+              child: GridView.builder(
+                itemCount: listProducts.length,
+                shrinkWrap: true,
+                physics: NeverScrollableScrollPhysics(),
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 3,
+                  childAspectRatio: 1.75,
+                ),
+                itemBuilder: (products, index) {
+                  var data = listProducts[index];
+                  return InkWell(
+                    splashColor: secondaryColor.withAlpha(30),
+                    onTap: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => (data['link'])));
+                    },
+                    child: Column(
+                      children: [
+                        Container(
+                          padding: EdgeInsets.all(0),
+                          margin: EdgeInsets.symmetric(horizontal: 5),
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.only(
+                                bottomLeft: Radius.circular(10),
+                                bottomRight: Radius.circular(10)),
+                            boxShadow: [
+                              BoxShadow(
+                                color: Color.fromRGBO(0, 0, 0, 0.1),
+                                blurRadius: 10,
+                                offset: Offset(0, 3),
+                              ),
+                            ],
+                          ),
+                          // padding:
+                          //     EdgeInsets.symmetric(vertical: 10, horizontal: 5),
+                          child: Column(children: [
+                            Image.network(
+                              data['image'],
+                              fit: BoxFit.contain,
+                            ),
+                            Container(
+                              padding: EdgeInsets.all(5),
+                              child: Column(
+                                children: [
+                                  Container(
+                                    margin: EdgeInsets.only(bottom: 10),
+                                    child: Text(
+                                      data['name'].toString().toUpperCase(),
+                                      textAlign: TextAlign.start,
+                                      style: TextStyle(
+                                        fontSize: 8.5,
+                                      ),
+                                    ),
+                                  ),
+                                  Container(
+                                      padding:
+                                          EdgeInsets.symmetric(horizontal: 4),
+                                      margin: EdgeInsets.only(bottom: 10),
+                                      child: Text(
+                                        data['price'],
+                                        textAlign: TextAlign.start,
+                                        style: TextStyle(
+                                            color: secondaryColor, fontSize: 9),
+                                      ))
+                                ],
+                              ),
+                            )
+                          ]),
+                        ),
+                      ],
+                    ),
+                  );
+                },
               ),
             ),
-            Center(child: Text('Belum ada apa-apa nih \n mulai belanja yuk')),
+
+            // Center(
+            //   child: Icon(
+            //     PhosphorIcons.clockCounterClockwiseBold,
+            //     size: 200,
+            //     color: secondaryColor,
+            //   ),
+            // ),
+            // Center(child: Text('Belum ada apa-apa nih \n mulai belanja yuk')),
           ],
-        ));
+        )));
   }
 }
--- lib/views/order/rincian_order.dart
+++ lib/views/order/rincian_order.dart
@@ -1,5 +1,8 @@
 import 'package:expektasi/core/utils/component.dart';
+import 'package:expektasi/core/utils/navigator_helper.dart';
+import 'package:expektasi/views/home/order_list.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:phosphor_flutter/phosphor_flutter.dart';
@@ -48,7 +51,7 @@ class _RincianOrderViewState extends State<RincianOrderView> {
               Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
             Text(
               "Menunggu Pembayaran",
-              style: TextStyle(fontSize: 12),
+              style: TextStyle(fontSize: 11),
             ),
             Padding(padding: EdgeInsets.symmetric(vertical: 20)),
             Row(
@@ -56,7 +59,7 @@ class _RincianOrderViewState extends State<RincianOrderView> {
               children: [
                 Text(
                   "Silahkan lakukan pembayaran paling\n lambat 28-07-2022 15:15 dengan\n Alfamart / Alfamidi / Dan+Dan",
-                  style: TextStyle(fontSize: 12),
+                  style: TextStyle(fontSize: 10),
                 ),
                 Icon(
                   PhosphorIcons.creditCardFill,
@@ -81,7 +84,7 @@ class _RincianOrderViewState extends State<RincianOrderView> {
               )),
               TextSpan(
                   text: "Alamat Pengiriman",
-                  style: TextStyle(fontSize: 13, color: Colors.black))
+                  style: TextStyle(fontSize: 12, color: Colors.black))
             ])),
             Padding(padding: EdgeInsets.symmetric(vertical: 10)),
             Text(
@@ -148,13 +151,158 @@ class _RincianOrderViewState extends State<RincianOrderView> {
                     Padding(padding: EdgeInsets.symmetric(horizontal: 3)),
                     Text(
                       "Kabari Penjual",
-                      style: TextStyle(color: secondaryColor, fontSize: 14),
+                      style: TextStyle(
+                          color: Color.fromARGB(255, 107, 107, 107),
+                          fontSize: 12),
+                    )
+                  ],
+                )),
+            Padding(padding: EdgeInsets.symmetric(vertical: 10)),
+            OutlinedButton(
+                onPressed: () {
+                  goPush(RincianOrderView2(), context);
+                },
+                style: OutlinedButton.styleFrom(
+                    backgroundColor: secondaryColor,
+                    side: BorderSide(color: secondaryColor, width: 2),
+                    padding: EdgeInsets.all(10)),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      "Bayar Sekarang",
+                      style: TextStyle(color: Colors.white, fontSize: 12),
                     )
                   ],
                 ))
           ]),
         )
       ]),
+    );
+  }
+}
+
+class RincianOrderView2 extends StatefulWidget {
+  const RincianOrderView2({Key? key}) : super(key: key);
+
+  @override
+  State<RincianOrderView2> createState() => _RincianOrderView2State();
+}
+
+class _RincianOrderView2State extends State<RincianOrderView2> {
+  Widget borderCustom() {
+    return Container(
+      decoration: BoxDecoration(
+          border: Border.all(color: Colors.grey.shade200, width: 3)),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        title: Text(
+          "Rincian Pesanan",
+          style: TextStyle(color: Colors.black),
+        ),
+        backgroundColor: Colors.white,
+        leading: IconButton(
+          icon: Icon(
+            Icons.arrow_back,
+            color: Colors.black,
+          ),
+          onPressed: () {
+            goPush(OrderViewList(0), context);
+          },
+        ),
+      ),
+      body: Container(
+        padding: EdgeInsets.symmetric(horizontal: 40, vertical: 10),
+        child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+          Container(
+            padding: EdgeInsets.symmetric(vertical: 20),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  "TOTAL PEMBAYARAN",
+                  style: TextStyle(fontSize: 11),
+                ),
+                Text(
+                  "Rp95.500",
+                  style: TextStyle(color: secondaryColor, fontSize: 11),
+                )
+              ],
+            ),
+          ),
+          borderCustom(),
+          Container(
+            padding: EdgeInsets.symmetric(vertical: 20),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  "BATAS WAKTU",
+                  style: TextStyle(fontSize: 11),
+                ),
+                Text(
+                  "15 jam 30 menit 0 detik",
+                  style: TextStyle(color: secondaryColor, fontSize: 11),
+                )
+              ],
+            ),
+          ),
+          borderCustom(),
+          Container(
+            padding: EdgeInsets.symmetric(vertical: 10),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Image.network(
+                  'https://cdn.discordapp.com/attachments/1008921423915532368/1012623387660664852/image_21.png',
+                  width: 80,
+                ),
+                Text(
+                  "Alfamart / Alfamidi / Dan+Dan",
+                  style: TextStyle(color: Colors.grey.shade400, fontSize: 11),
+                )
+              ],
+            ),
+          ),
+          Container(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  "KODE PEMBAYARAN",
+                  style: TextStyle(fontSize: 9),
+                ),
+                TextButton(
+                  onPressed: () {
+                    Clipboard.setData(
+                        ClipboardData(text: "EXPT U0JF 7GNK 765"));
+                  },
+                  child: Text(
+                    "SALIN",
+                    style: TextStyle(color: Colors.greenAccent, fontSize: 8),
+                  ),
+                )
+              ],
+            ),
+          ),
+          Container(
+            width: 300,
+            padding: EdgeInsets.symmetric(vertical: 10),
+            alignment: Alignment.center,
+            decoration: BoxDecoration(color: secondaryColor),
+            child: SelectableText(
+              "EXPT U0JF 7GNK 765",
+              style: TextStyle(color: Colors.white, fontSize: 15),
+            ),
+          )
+        ]),
+      ),
     );
   }
 }
